837cc87ccb5e559eb7aa5de93d9de34bf76ce458
mukesh12 <er.mukeshpatil2018@gmail.com>
2023-07-15 12:22:19 +0530

    Deploy testing - 08

--- per-portfolio/src/App.js
+++ per-portfolio/src/App.js
@@ -1,5 +1,5 @@
 import NavBar from "./components/NavBar";
-import { Routes, Route, HashRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Footer from "./components/Footer";
 import Home from "./components/Home";
 import About from "./components/About";
@@ -11,9 +11,9 @@ function App() {
     <div className="flex items-center flex-col text-dark bg-light w-full min-h-screen">
       <NavBar />
       {/* <Layout className="pt-0 mb-8"> */}
-      <HashRouter>
+      <BrowserRouter>
         <Routes>
-          <Route index path="/personal-Portfolio" element={<Home />} />
+          <Route path="/personal-Portfolio" element={<Home />} />
           <Route path="/personal-Portfolio/about" element={<About />} />
           <Route
             path="/personal-Portfolio/experience"
@@ -21,7 +21,7 @@ function App() {
           />
           <Route path="/personal-Portfolio/education" element={<Education />} />
         </Routes>
-      </HashRouter>
+      </BrowserRouter>
       {/* <Home /> */}
       {/* </Layout> */}
       <Footer />
